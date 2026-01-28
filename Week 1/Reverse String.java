e7eaf1e47236229fb9bba785b103e7fa5bc7cee2
padmini-ux02 <vtu24646@veltech.edu.in>
2026-01-28 12:21:26 +0530

    Update Reverse String.java

--- Week 1/Reverse String.java
+++ Week 1/Reverse String.java
@@ -1,9 +1,12 @@
-class Solution {
-    public void reverseString(char[] s) {
+class Solution 
+{
+    public void reverseString(char[] s) 
+    {
         int left = 0;
         int right = s.length - 1;
 
-        while(left < right){
+        while(left < right)
+        {
             char temp = s[left];
             s[left] = s[right];
             s[right] = temp;
