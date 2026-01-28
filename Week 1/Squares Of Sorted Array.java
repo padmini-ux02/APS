f4d59a17f6fda517b8ce9c6b5793d83c2ba28870
padmini-ux02 <vtu24646@veltech.edu.in>
2026-01-28 12:22:34 +0530

    Update Squares Of Sorted Array.java

--- Week 1/Squares Of Sorted Array.java
+++ Week 1/Squares Of Sorted Array.java
@@ -1,14 +1,20 @@
-class Solution {
-    public int[] sortedSquares(int[] nums) {
+class Solution 
+{
+    public int[] sortedSquares(int[] nums) 
+    {
         int[] sol = new int[nums.length];
         int left = 0;
         int right = nums.length - 1;
 
-        for(int i = nums.length -1; i >= 0; i--){
-            if (Math.abs(nums[left]) > Math.abs(nums[right])){
+        for(int i = nums.length -1; i >= 0; i--)
+        {
+            if (Math.abs(nums[left]) > Math.abs(nums[right]))
+            {
                 sol[i] = nums[left] * nums[left];
                 left++;
-            }else{
+            }
+            else
+            {
                 sol[i] = nums[right] * nums[right];
                 right--;
             }
