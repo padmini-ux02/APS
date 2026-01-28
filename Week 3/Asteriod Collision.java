aebf666af72d3dad8817ca48143364155d2c7859
padmini-ux02 <vtu24646@veltech.edu.in>
2026-01-28 12:29:15 +0530

    Update Asteriod Collision.java

--- Week 3/Asteriod Collision.java
+++ Week 3/Asteriod Collision.java
@@ -1,31 +1,42 @@
 import java.util.*;
 
-class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
+class Solution 
+{
+    public int[] asteroidCollision(int[] asteroids) 
+    {
         Stack<Integer> stack = new Stack<>();
 
-        for (int a : asteroids) {
+        for (int a : asteroids) 
+        {
             boolean alive = true;
 
-            while (alive && a < 0 && !stack.isEmpty() && stack.peek() > 0) {
+            while (alive && a < 0 && !stack.isEmpty() && stack.peek() > 0) 
+            {
                 int top = stack.peek();
-                if (top < -a) {
+                if (top < -a) 
+                {
                     stack.pop();        
-                } else if (top == -a) {
+                }
+                else if (top == -a) 
+                {
                     stack.pop();        
                     alive = false;
-                } else {
+                }
+                else 
+                {
                     alive = false;     
                 }
             }
 
-            if (alive) {
+            if (alive) 
+            {
                 stack.push(a);
             }
         }
 
         int[] result = new int[stack.size()];
-        for (int i = stack.size() - 1; i >= 0; i--) {
+        for (int i = stack.size() - 1; i >= 0; i--) 
+        {
             result[i] = stack.pop();
         }
 
