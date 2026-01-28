ab23a08ced286ea57e096812f65ae17f438a3c8c
padmini-ux02 <vtu24646@veltech.edu.in>
2026-01-28 12:18:46 +0530

    Update Binary Search.java

--- Week 1/Binary Search.java
+++ Week 1/Binary Search.java
@@ -1,9 +1,12 @@
-class Solution {
-    public int search(int[] nums, int target) {
+class Solution 
+{
+    public int search(int[] nums, int target) 
+    {
         int left = 0;
         int right = nums.length - 1;
 
-        while(left <= right){
+        while(left <= right)
+        {
             int mid = left + (right-left) / 2;
             if(nums[mid] == target)
                 return mid;
