b1f2222ed9aee28abdd4d5df939f1b7f98938a17
padmini-ux02 <vtu24646@veltech.edu.in>
2026-01-28 12:21:55 +0530

    Update Running Sum of 1D Array.java

--- Week 1/Running Sum of 1D Array.java
+++ Week 1/Running Sum of 1D Array.java
@@ -1,7 +1,10 @@
-class Solution {
-    public int[] runningSum(int[] nums) {
+class Solution 
+{
+    public int[] runningSum(int[] nums) 
+    {
         int sum = 0;
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++)
+        {
             sum += nums[i];
             nums[i] = sum; 
         }
