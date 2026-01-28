36f40aab4ec857109649e5fc91f6ae06a8ba722d
padmini-ux02 <vtu24646@veltech.edu.in>
2026-01-28 12:19:50 +0530

    Update Contains Duplicates - II.java

--- Week 1/Contains Duplicates - II.java
+++ Week 1/Contains Duplicates - II.java
@@ -1,13 +1,18 @@
 import java.util.*;
 
-class Solution {
-    public boolean containsNearbyDuplicate(int[] n, int k) {
+class Solution 
+{
+    public boolean containsNearbyDuplicate(int[] n, int k) 
+    {
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < n.length; i++){
-            if(i > k){
+        for (int i = 0; i < n.length; i++)
+        {
+            if(i > k)
+            {
                 set.remove(n[i-k-1]);
             }
-            if(!set.add(n[i])){
+            if(!set.add(n[i]))
+            {
                 return true;
             }
         }
