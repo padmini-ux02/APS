7909fc08b36990be3256119142d8ff4c5513b5af
padmini-ux02 <vtu24646@veltech.edu.in>
2026-01-28 12:32:58 +0530

    Update Online Stock Span.java

--- Week 3/Online Stock Span.java
+++ Week 3/Online Stock Span.java
@@ -1,16 +1,20 @@
 import java.util.Stack;
 
-class StockSpanner {
+class StockSpanner
+    {
     private Stack<int[]> stack;
 
-    public StockSpanner() {
+    public StockSpanner()
+        {
         stack = new Stack<>();
     }
     
-    public int next(int price) {
+    public int next(int price) 
+        {
         int span = 1;
 
-        while (!stack.isEmpty() && stack.peek()[0] <= price){
+        while (!stack.isEmpty() && stack.peek()[0] <= price)
+        {
             span += stack.pop()[1];
         }
         stack.push(new int[]{price,span});
