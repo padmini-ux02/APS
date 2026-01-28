d5295711eb89df250706095bc1ef2b25b6469962
padmini-ux02 <vtu24646@veltech.edu.in>
2026-01-28 12:31:59 +0530

    Update Minimum Remove to make valid Parentheses.java

--- Week 3/Minimum Remove to make valid Parentheses.java
+++ Week 3/Minimum Remove to make valid Parentheses.java
@@ -1,18 +1,27 @@
-class Solution {
-    public String minRemoveToMakeValid(String s) {
+class Solution 
+{
+    public String minRemoveToMakeValid(String s) 
+    {
         StringBuilder firstPass = new StringBuilder();
         int open = 0;
 
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
+        for (char c : s.toCharArray()) 
+        {
+            if (c == '(') 
+            {
                 open++;
                 firstPass.append(c);
-            } else if (c == ')') {
-                if (open > 0) {
+            } 
+            else if (c == ')')
+            {
+                if (open > 0) 
+                {
                     open--;
                     firstPass.append(c);
                 }
-            } else {
+            } 
+            else
+            {
                 firstPass.append(c);
             }
         }
@@ -21,17 +30,23 @@ class Solution {
         int close = 0;
 
         
-        for (int i = firstPass.length() - 1; i >= 0; i--) {
+        for (int i = firstPass.length() - 1; i >= 0; i--) 
+        {
             char c = firstPass.charAt(i);
-            if (c == ')') {
+            if (c == ')') 
+            {
                 close++;
                 result.append(c);
-            } else if (c == '(') {
-                if (close > 0) {
+            } else if (c == '(')
+            {
+                if (close > 0)
+                {
                     close--;
                     result.append(c);
                 }
-            } else {
+            } 
+            else
+            {
                 result.append(c);
             }
         }
