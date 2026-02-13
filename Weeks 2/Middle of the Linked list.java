bb0e1c2a089193cf4bbc637ea9251ef0ac1eb666
padmini-ux02 <vtu24646@veltech.edu.in>
2026-02-13 09:44:54 +0530

    Update Middle of the Linked list.java

--- Weeks 2/Middle of the Linked list.java
+++ Weeks 2/Middle of the Linked list.java
@@ -1,39 +1,16 @@
-class ListNode 
-{
-
-    int val;
-    ListNode next;
-
-    ListNode() 
-    {
-    }
-
-    ListNode(int val) 
-    {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) 
-    {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 class Solution 
 {
-
-    public ListNode middleNode(ListNode head) 
+    public ListNode middleNode(ListNode head)
     {
-        ListNode a = head;
-        ListNode b = head;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while (b != null && b.next != null) 
+        while (fast != null && fast.next != null) 
         {
-            a = a.next;
-            b = b.next.next;
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        return a;
+        return slow;   // will point to 4 for this list
     }
 }
