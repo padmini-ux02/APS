0efc094644ba2496f3e070a989e63901585fdb37
Padmini <254588551+padmini-ux02@users.noreply.github.com>
2026-04-01 11:39:17 +0530

    APS

--- Week 4/Task 9.java
+++ Week 4/Task 9.java
@@ -1,17 +1,21 @@
 import java.util.*;
 
-class RecentCounter {
+class RecentCounter 
+{
 
     Queue<Integer> queue;
 
-    public RecentCounter() {
+    public RecentCounter() 
+    {
         queue = new LinkedList<>();
     }
     
-    public int ping(int t) {
+    public int ping(int t) 
+    {
         queue.offer(t);
         
-        while (!queue.isEmpty() && queue.peek() < t - 3000) {
+        while (!queue.isEmpty() && queue.peek() < t - 3000) 
+        {
             queue.poll();
         }
         
